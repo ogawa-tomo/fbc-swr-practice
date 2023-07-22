4b092319e580adf00b6f1bacd510e5c5eb993da1
tomo <o_tomo_1990@yahoo.co.jp>
2023-07-22 19:43:27 +0900

    引数の移動

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import useSWR from "swr";
 import "./App.css";
 
-const fetcher = (url) => {
-  const headers = { Accept: "application/json" }
+const fetcher = async (url, headers) => {
   return fetch(url, { headers }).then((res) => res.json());
 }
 
-function App() {
+const useStatus = () => {
   const url = "https://httpstat.us/200?sleep=2000";
+  const headers = { Accept: "application/json" };
+  const { data, error } = useSWR([url, headers], ([url, headers]) => fetcher(url, headers));
+  return { data, error }
+}
 
-  const { data, error } = useSWR(url, fetcher);
+function App() {
+  const { data, error } = useStatus()
 
   if (error) return <p>Failed</p>
   if (!data) return <p>Loading</p>
