e1d58efb3c9df1bb969d26ff3cda75bf0c484cce
tomo <o_tomo_1990@yahoo.co.jp>
2023-07-22 20:18:57 +0900

    微修正

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,28 @@ import "./App.css";
 
 const fetcher = async (url, headers) => {
   return fetch(url, { headers }).then((res) => res.json());
-}
+};
 
 const useStatus = () => {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
-  const { data, error } = useSWR([url, headers], ([url, headers]) => fetcher(url, headers));
-  return { data, error }
-}
+  const { data, error, isLoading } = useSWR([url, headers], ([url, headers]) =>
+    fetcher(url, headers)
+  );
+  return {
+    status: data?.description,
+    isError: error,
+    isLoading,
+  };
+};
 
 function App() {
-  const { data, error } = useStatus()
+  const { status, isError, isLoading } = useStatus();
 
-  if (error) return <p>Failed</p>
-  if (!data) return <p>Loading</p>
+  if (isError) return <p>Failed</p>;
+  if (isLoading) return <p>Loading</p>;
 
-  return <p>{data.description}</p>
+  return <p>Status: {status}</p>;
 }
 
 export default App;
